6a193138f45aacb2d0382b3ce409a10a52158a1f
Michelle <161183828+SpookyCthulhu@users.noreply.github.com>
2025-02-17 04:59:19 -0500

    Adding back random country colors

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import Map from "./components/Map";
 import MapViewer from "./components/MapViewer";
@@ -12,6 +12,27 @@ const Home = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [countryData, setCountryData] = useState(null);
   const [loading, setLoading] = useState(false);
+
+
+  useEffect(() => {
+    const countries = document.querySelectorAll(".Country");
+
+    countries.forEach(country => {
+        // Generate escaped class selector for the country
+        const classes = [...country.classList].map(c => CSS.escape(c)).join('.');
+        const selector = `${classes.split(".")[1]}`;
+        console.log(selector);
+        
+        // Generate ONE random color per country
+        const randomColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+        console.log(randomColor);
+        // Apply color to all matching elements
+        document.querySelectorAll("."+selector).forEach(el => {
+            console.log("visiting " + selector)
+            el.style.fill = randomColor;
+        });
+      });
+  }, );
 
   const handleCountryClick = async (countryName) => {
 
